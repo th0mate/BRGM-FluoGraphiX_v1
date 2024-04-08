383cc66415940b82a1a2013498bbb8d78e959b64
Thomas Loye <thomas.loye@etu.umontpellier.fr>
2024-04-08 09:52:12 +0200

    feat : init github

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,6 +22,14 @@ module.exports = {
             h1: {
                 'font-size': '2.25rem',
                 'line-height': '2.5rem',
+            },
+
+            screens: {
+                'sm': {'max' : '640px'},
+                'md': {'max' : '768px'},
+                'lg': {'max' : '1024px'},
+                'xl': {'max' : '1280px'},
+                '2xl': {'max' : '1536px'},
             }
         },
     },
