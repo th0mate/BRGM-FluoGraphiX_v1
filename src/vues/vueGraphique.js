512e11171de9bd2681f99788ec8c43d674248d48
Thomas Loye <thomas.loye@etu.umontpellier.fr>
2024-04-11 12:35:07 +0200

    rebase : messagesFlash et popup centrés

--- src/vues/vueGraphique.js
+++ src/vues/vueGraphique.js
@@ -1,7 +1,6 @@
 
 function vueGraphique() {
     return `<input type="file" id="fileInput" multiple onchange="traiterFichier()">
-    <!--<button class="bouton boutonFonce" onclick="resetZoom()">RÉINITIALISER LE ZOOM</button>-->
     <button onclick="resetZoom()">RÉINITIALISER LE ZOOM</button>
     <button class="downloadFile" onclick="telechargerFichier()">Télécharger le fichier</button>
     <label for="xZoom">Zoom sur axe X</label>
@@ -11,12 +10,12 @@ function vueGraphique() {
     
 
     <label for="selectFormatDate">Format Date</label><select onchange="modifierFormat(this.value)" id="selectFormatDate">
-    <option value="1">jj/mm/aa</option>
-    <option value="2">aa/mm/jj</option>
-    <option disabled value="0">Automatique</option>
-</select>
+        <option value="1">jj/mm/aa</option>
+        <option value="2">aa/mm/jj</option>
+        <option disabled value="0">Automatique</option>
+    </select>
     <br>
-        <div class="graphiques">
-            <canvas class="graphique" id="graphique"></canvas>
-        </div>`;
+    <div class="graphiques">
+        <canvas class="graphique" id="graphique"></canvas>
+    </div>`;
 }
