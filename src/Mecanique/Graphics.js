668fb5e10fa1e0779eb1d482b456432a8b65734e
Thomas Loye <thomas.loye@etu.umontpellier.fr>
2024-04-11 17:27:36 +0200

    feat : avancées visuelles

--- src/Mecanique/Graphics.js
+++ src/Mecanique/Graphics.js
@@ -231,6 +231,7 @@ function afficherGraphique(mvContent) {
 
 
     cacherDoublons();
+    document.querySelector('.resetZoom').style.display = 'flex';
 }
 
 
--- src/vues/vueGraphique.js
+++ src/vues/vueGraphique.js
@@ -1,21 +1,54 @@
 
 function vueGraphique() {
-    return `<input type="file" id="fileInput" multiple onchange="traiterFichier()">
-    <button onclick="resetZoom()">RÉINITIALISER LE ZOOM</button>
-    <button class="downloadFile" onclick="telechargerFichier()">Télécharger le fichier</button>
-    <label for="xZoom">Zoom sur axe X</label>
-    <input type="checkbox" checked id="xZoom" onclick="modifierZoom('x')">
-    <label for="yZoom">Zoom sur axe Y</label>
-    <input type="checkbox" checked id="yZoom" onclick="modifierZoom('y')">
+    return `
     
-
-    <label for="selectFormatDate">Format Date</label><select onchange="modifierFormat(this.value)" id="selectFormatDate">
-        <option value="1">jj/mm/aa</option>
-        <option value="2">aa/mm/jj</option>
-        <option disabled value="0">Automatique</option>
-    </select>
     <br>
     <div class="graphiques">
         <canvas class="graphique" id="graphique"></canvas>
-    </div>`;
+        
+        <div class="settings">
+            <div class="entete">
+                <h2>Paramètres</h2>
+                <img src="Ressources/img/reduire.png" alt="Réduire" onclick="reduireSettings()">
+        </div>
+        
+        <div class="details">
+            <h3>Charger d'autres fichiers</h3>
+            <div>
+                <input type="file" id="fileInput" multiple onchange="traiterFichier()">
+            </div>
+            
+            <h3>Gestion du Zoom</h3>
+            <div>
+                <label>Zoom sur axe X
+                <input type="checkbox" checked id="xZoom" onclick="modifierZoom('x')">
+                </label>
+                <label>Zoom sur axe Y
+                <input type="checkbox" checked id="yZoom" onclick="modifierZoom('y')">
+                </label>
+            </div>
+            
+            <h3>Format de dates</h3>
+            <div>
+                <label>Format :<select onchange="modifierFormat(this.value)" id="selectFormatDate">
+                    <option value="1">jj/mm/aa</option>
+                    <option value="2">aa/mm/jj</option>
+                    <option disabled value="0">Automatique</option>
+                </select>
+                </label>
+            </div>
+            
+            <div>
+                <div class="bouton boutonFonce resetZoom" onclick="resetZoom()">Réinitialiser le zoom</div>
+             
+                <div class="bouton boutonFonce downloadFile" onclick="telechargerFichier()">TÉLÉCHARGER LE FICHIER</div>
+                
+            </div>
+        
+        </div>
+    </div>
+</div>
+    
+    
+`;
 }
