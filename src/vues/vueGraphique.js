8a6c60f5312c755aa7b9b3ee76b9f04fe7b4beac
Thomas Loye <thomas.loye@etu.umontpellier.fr>
2024-04-11 20:03:34 +0200

    feat : possibilité de réduire les paramètres du graphique

--- src/vues/vueGraphique.js
+++ src/vues/vueGraphique.js
@@ -1,4 +1,3 @@
-
 function vueGraphique() {
     return `
     
@@ -43,12 +42,52 @@ function vueGraphique() {
              
                 <div class="bouton boutonFonce downloadFile" onclick="telechargerFichier()">TÉLÉCHARGER LE FICHIER</div>
                 
-            </div>
-        
+            </div>        
         </div>
     </div>
 </div>
-    
+
+    <div class="arrow" onclick="agrandirSettings()">
+        <img src="Ressources/img/flecheOrange.png" alt="Flèche">
+    </div>
     
 `;
+}
+
+let html;
+
+/**
+ * Réduit la taille des paramètres pour afficher le graphique en plus grand
+ */
+function reduireSettings() {
+    const graphique = document.querySelector('.graphique');
+    const settings = document.querySelector('.settings');
+
+    graphique.style.width = '100%';
+    graphique.style.transition = 'width 0.5s ease';
+    settings.style.width = '0';
+    settings.style.overflow = 'hidden';
+    settings.style.transition = 'width 0.5s ease';
+    html = settings.innerHTML;
+    settings.innerHTML = '';
+
+    setTimeout(() => {
+        document.querySelector('.arrow').style.display = 'flex';
+    }, 450);
+}
+
+/**
+ * Agrandit la taille des paramètres pour afficher le graphique en plus petit
+ */
+function agrandirSettings() {
+    const graphique = document.querySelector('.graphique');
+    const settings = document.querySelector('.settings');
+
+    graphique.style.maxwidth = '70%';
+    graphique.style.transition = 'width 0.5s ease';
+    settings.style.width = '300px';
+    settings.style.overflow = 'auto';
+    settings.style.transition = 'width 0.5s ease';
+    document.querySelector('.arrow').style.display = 'none';
+    settings.innerHTML = html;
 }
