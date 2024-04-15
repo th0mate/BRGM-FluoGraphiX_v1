a7754fb643e35fb87a9c5b6c563a5ec0591b6392
Thomas Loye <thomas.loye@etu.umontpellier.fr>
2024-04-15 12:31:44 +0200

    feat : avancées mentions légales

---
+++ src/vues/vueMentionsLegales.js
@@ -0,0 +1,51 @@
+function vueMentionsLegales() {
+    return`
+    <div class="bannierePage">
+        <img src="Ressources/img/image3.jpg" alt="Sources">
+        <div class="contenu">
+            <p><span onclick="afficherVue('vueAccueil')"><span></span>Accueil</span> <strong> / Mentions Légales</strong></p>
+            <h1>Mentions Légales</h1>
+        </div>
+    </div>
+    
+    <div class="cookies">
+    <h2>En utilisant ce site internet, vous acceptez les conditions générales d'utilisation énoncées ci-après. Le BRGM se réserve le droit de modifier les présentes dispositions à tout moment et sans avis préalable.</h2>
+    
+    <h1>Éditeur du site</h1>
+    <p>Ce site internet est édité par le BRGM (Bureau de Recherches Géologiques et Minières). Le BRGM est un établissement public à caractère industriel et commercial (EPIC).</p>
+    <h3>Équipe en charge du site</h3>
+    <h4>Directeur de la publication</h4>
+    <p>Pierre Vassal, responsable de la Communication, de la Médiation scientifique et des Éditions du BRGM</p>
+    
+    <h4>Responsable éditoriale</h4>
+    <p>Aude Rouger-Loiseau, chargée de la communication digitale, service de la Communication de la Médiation scientifique et des Éditions</p>
+    
+    <h4>Webmaster technique</h4>
+    <p>Fabrice Catonet, responsable de l'unité Équipements et services aux utilisateurs, secrétariat général, service Systèmes d'information</p>
+    
+    <h3>Coordonnées</h3>
+    <p>BRGM</p>
+    <p>3 avenue Claude-Guillemin, BP 36009</p>
+    <p>45060 Orléans Cedex 02</p>
+    <p>France</p>
+    <br>
+    <p>Tél: + 33 (0)2 38 64 34 34</p>
+    
+    
+    <h3>Renseignements administratifs</h3>
+    <p>Code APE (NAF) : 7219Z</p>
+    <p>Numéro SIREN : 582 056 149</p>
+    <p>Numéro SIRET (siège) : 58205614900120</p>
+    <p>Numéro RCS : RCS Orléans 582 056 149</p>
+    <p>TVA intracommunautaire : FR67582056149</p>
+    
+    <h1>Crédits</h1>
+    <h3>Conception, création et développement du site</h3>
+    <p>Thomas Loye</p>
+    <a class="bouton boutonFonce" href="https://thomasloye.fr">Site Internet</a>
+    <a class="bouton boutonClair" href="https://thomasloye.fr">Linkedin</a>
+    
+    </div>
+    
+    `;
+}
