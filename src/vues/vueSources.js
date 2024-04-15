c48cb33f896910bdc9bdeaee04d425da712bb49d
Thomas Loye <thomas.loye@etu.umontpellier.fr>
2024-04-15 11:43:36 +0200

    feat : sources

---
+++ src/vues/vueSources.js
@@ -0,0 +1,25 @@
+function vueSources() {
+    return `
+    <div class="bannierePage">
+        <img src="Ressources/img/image2.jpg" alt="Sources">
+        <div class="contenu">
+            <p><span onclick="afficherVue('vueAccueil')"><span></span>Accueil</span> <strong> / Sources du Contenu</strong></p>
+            <h1>Sources du Contenu</h1>
+        </div>
+    </div>
+    <div class="sources">
+        <h1>Crédits Photos</h1>
+        <p>Le crédit photo des images utilisées comme bannières dans les pages est réputé être le suivant : © BRGM</p>
+        <p>D'autres photos et illustrations sont utilisées ponctuellement dans certaines pages. Elles portent notamment les crédits suivants :</p>
+        <ul>
+            <li>© Storyset</li>
+            <li>© Loading.io</li>
+            <li>© Pana sur Storyset</li>
+            <li>© Cuate sur Storyset</li>
+            <li>© Freepik sur Flaticon</li>
+            <li>© Anggara sur Flaticon</li>
+            <li>© Smashicons sur Flaticon</li>
+        </ul>
+    </div>
+    `;
+}
