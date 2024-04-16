46f5efd8ef4cf85da36416d60bd06ab14f05e1b9
Thomas Loye <thomas.loye@etu.umontpellier.fr>
2024-04-16 15:49:57 +0200

    feat : avancées

--- src/vues/vueAccueil.js
+++ src/vues/vueAccueil.js
@@ -10,28 +10,51 @@ function vueAccueil() {
     
     <div class="accueil">
     
-    <div class="text-container"><h1 class="titreBarre">Actions Rapides</h1></div>
+        <div class="text-container"><h1 class="titreBarre">Actions Rapides</h1></div>
     
-        <div class="actionsRapides">
-            <div class="action">
-                <img src="Ressources/img/grandGraphique.png" alt="icone">
-                <h3>Visualisez les données de vos fichiers sous la forme de graphiques</h3>
-                <span></span>
-            </div>
+            <div class="actionsRapides">
+                <div class="action" onclick="afficherVue('vueGraphique')">
+                    <img src="Ressources/img/grandGraphique.png" alt="icone">
+                    <h3>Visualisez les données de vos fichiers sous la forme de graphiques</h3>
+                    <span></span>
+                </div>
             
-            <div class="action">
-                <img src="Ressources/img/grandsCalculs.png" alt="icone">
-                <h3>Calculez des données de concentrations à partir d'un fichier Calibrat.dat</h3>
-                <span></span>
-            </div>
+                <div class="action" onclick="afficherVue('vueConcentrations')">
+                    <img src="Ressources/img/grandsCalculs.png" alt="icone">
+                    <h3>Calculez des données de concentrations à partir d'un fichier Calibrat.dat</h3>
+                    <span></span>
+                </div>
             
-            <div class="action">
-                <img src="Ressources/img/grandeAide.png" alt="icone">
-                <h3>Consultez la documentation pour obtenir des réponses à vos questions</h3>
-                <span></span>
-            </div>
+                <div class="action" onclick="afficherVue('vueDocumentation')">
+                    <img src="Ressources/img/grandeAide.png" alt="icone">
+                    <h3>Consultez la documentation pour obtenir des réponses à vos questions</h3>
+                    <span></span>
+                </div>
            
-        </div>
+            </div>
+            
+            
+            
+            <div class="text-container"><h1 class="titreBarre">Visualisation de données</h1></div>
+            <div class="wrapAccueil">
+                <div>
+                    <h1>Facilité d'utilisation</h1>
+                    <p>Sélectionnez un ou plusieurs fichiers, puis laissez la magie opérer. En l'espace de trois secondes, vos données sont affichées, et c'est un jeu d'enfant.</p>
+                    <h1>Personnalisation</h1>
+                    <p>Personnalisez tous les paramètres dont vous avez besoin dans le bandeau de la page dédié.</p>
+                </div>
+                <img src="Ressources/img/screenGraphique.png" alt="">
+            </div>
+            
+            
+            <div class="text-container"><h1 class="titreBarre">Automatisation d'actions</h1></div>
+            <div class="wrapAccueil">
+                <div>
+                    <h1>Format de dates automatique</h1>
+                    <p>En joignant votre fichier Calibrat.dat, FluoriGraphix en déduit automatiquement quel format de date appliquer à vos données. Vous pouvez également configurer ce paramètre manuellement.</p>
+                </div>
+            </div>
+            
         
     </div>
     
