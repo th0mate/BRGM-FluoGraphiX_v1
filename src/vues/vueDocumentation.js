f8f1e125c5d6d5a7a3f91b43c864b2b68270b884
Thomas Loye <thomas.loye@etu.umontpellier.fr>
2024-04-24 09:09:00 +0200

    feat : init doc

---
+++ src/vues/vueDocumentation.js
@@ -0,0 +1,17 @@
+function vueDocumentation() {
+    return `
+    <div class="bannierePage">
+        <img src="Ressources/img/image6.jpg" alt="Sources">
+        <div class="contenu">
+            <p><span onclick="afficherVue('vueAccueil')"><span></span>Accueil</span> <strong> / Documentation</strong></p>
+            <h1>Documentation</h1>
+        </div>
+    </div>
+    
+    
+    <div class="documentation">
+    
+    </div>
+    
+    `;
+}
