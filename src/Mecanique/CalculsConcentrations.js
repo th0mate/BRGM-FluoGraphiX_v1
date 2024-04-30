53fbc42cc3ef948457a625d34ef9ac3cd51f10d1
Thomas Loye <thomas.loye@etu.umontpellier.fr>
2024-04-30 17:33:06 +0200

    feat : init courbes. Fin de journée

--- src/Mecanique/CalculsConcentrations.js
+++ src/Mecanique/CalculsConcentrations.js
@@ -1,36 +1,47 @@
-
-
 let nbValeurLampe = 0;
+
 /**
  * Effectue toutes les inits et les calculs nécessaires pour le calcul des concentrations d'un traceur et d'une lampe donnés
  * @param idLampe l'id de la lampe
  * @param traceur le traceur
  */
 function calculerConcentration(idLampe, traceur) {
-    console.log(traceur.echelles);
     nbValeurLampe = 0;
     for (let i = 0; i < traceur.echelles.length; i++) {
         if (!isNaN(traceur.getDataParNom('L' + idLampe + '-' + (i + 1)))) {
             nbValeurLampe++;
         }
     }
-    console.log(nbValeurLampe);
 
     const final = new Map();
     let resultat = [];
-    if (nbValeurLampe < 4 && nbValeurLampe !== 1) {
+
+
+    if (nbValeurLampe < 4 && nbValeurLampe !== 1) { //TODO - erreur pour certains où n === 3 !!!
         const dmV = creerTableauValeursNettes(traceur, idLampe);
         let X = creerMatriceLn(traceur, dmV);
         X = inverse(X);
         const matriceEntetes = dmV[0];
         resultat = multiply([matriceEntetes], X);
 
+
     } else if (nbValeurLampe === 1) {
         const eau = traceurs.find(traceur => traceur.unite === '');
         const dmv = [];
+        let temp = 0;
+        const y = [];
+        y.push(0);
         dmv.push(0);
-        dmv.push(traceur.getDataParNom('L' + idLampe + '-1') - eau.getDataParNom('L' + idLampe + '-1'));
-        console.log(dmv);
+        for (let i = 1; i <= traceur.echelles.length; i++) {
+            if (!isNaN(traceur.getDataParNom('L' + idLampe + '-' + i))) {
+                temp = traceur.getDataParNom('L' + idLampe + '-' + i);
+                y.push(traceur.echelles[i - 1]);
+            }
+        }
+        dmv.push(temp - eau.getDataParNom('L' + idLampe + '-1'));
+        resultat.push(arrondir8Chiffres((y[1] - y[0]) / (dmv[1] - dmv[0])));
+        console.log(resultat);
+        return resultat;
 
     } else {
         const regLin = creerTableauValeursNettesLn(traceur, idLampe);
@@ -49,7 +60,7 @@ function calculerConcentration(idLampe, traceur) {
             colonne2_3.push([1, colonne2[i], colonne3[i]]);
         }
 
-        resultat = multipleLinearRegression(colonne2_3,[colonne1]);
+        resultat = multipleLinearRegression(colonne2_3, [colonne1]);
         resultat = transpose(resultat);
 
     }
@@ -58,6 +69,12 @@ function calculerConcentration(idLampe, traceur) {
         final.set('Degré 1', arrondir8Chiffres(resultat[0][1]));
         final.set('Degré 2', arrondir8Chiffres(resultat[0][2]));
         console.log(final);
+
+        //TODO : dans le graphique du canvas #graphiqueTraceur, on affiche la courbe selon la formule y= exp(a + b ln(X-X0) + c (ln(X-X0))^2), avec X0 la valeur de l'eau pour la lampe, et a, b et c les valeurs calculées
+        const eau = traceurs.find(traceur => traceur.unite === '');
+
+
+
         return final;
     }
     return null;
@@ -99,7 +116,6 @@ function creerTableauValeursNettes(traceur, lampe) {
 }
 
 
-
 /**
  * Retourne un tableau (matrice) contenant les signaux nets pour un traceur et une lampe donnée.
  * Les titres des colonnes sont les logarithmes népériens des échelles du traceur
@@ -131,7 +147,6 @@ function creerTableauValeursNettesLn(traceur, lampe) {
 }
 
 
-
 /**
  * Crée une matrice contenant ln(creerTableauValeursNettes)** position de chaque cellule de traceur.echelles
  */
@@ -147,8 +162,6 @@ function creerMatriceLn(traceur, tableauValeursNettes) {
     }
     return matriceLn;
 }
-
-
 
 
 /**
