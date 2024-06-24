bb8929bf125c0ca7976239fad12cf197d3508147
Thomas Loye <thomas.loye@etu.umontpellier.fr>
2024-06-24 19:23:50 +0200

    rebase : fin refonte vueTelecharger.js

--- src/vues/vueTelecharger.js
+++ src/vues/vueTelecharger.js
@@ -21,12 +21,21 @@ function vueTelecharger() {
     -->
     
     <div class="partOne">
-        <h1 class="titreBarre">Téléchargez FluoGraphiX</h1>
-        <h3>Profitez d'un outil complet et polyvalent, proposant de nouvelles fonctionnalités</h3>
+        <h1 class="titreBarre">Télécharger FluoGraphiX</h1>
+        <h3>Profitez d'un outil complet et polyvalent, une solution innovante pour visualiser et manipuler vos données de mesures et de calibration issues de Fluorimètres.</h3>
+        <p>Visualisation de données, vérification de données de calibration, calcul de courbes de calibration, conversion en concentration, correction de l'incidence de la turbidité, correction des interférences entre traceurs, correction du bruit de fond...</p>
+        <p>En téléchargeant FluoGraphiX sur votre appareil, vous pouvez profiter de toutes les fonctionnalités de l'outil partout, tout le temps : que ce soir sur mobile, sur tablette, ou encore sur ordinateur.</p>
+        <div>
+            <img src="Ressources/img/rocket.png" alt="Fusée">
+            <h2>Rejoignez l'aventure FluoGraphiX !</h2>
+            <div class="boutonOrange" onclick="downloadSite()">TÉLÉCHARGER MAINTENANT</div>
+        </div>
     </div>
     
     <div class="partTwo">
-    
+        <img src="Ressources/img/ipad.png" alt="PC">
+        <img src="Ressources/img/imac.png" alt="PC">
+        <img src="Ressources/img/iphone.png" alt="PC">
     </div>
     
         
